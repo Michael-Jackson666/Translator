e8e7e9f83f43506baa72d301f8e9cb8dd3b382b4
Jack <jackson11235813@163.com>
2025-09-08 11:16:54 +0800

    learn GELU for feed-forward network

--- llm-scratch/chap4-implement_gpt_model/feed-forward.py
+++ llm-scratch/chap4-implement_gpt_model/feed-forward.py
@@ -1,5 +1,4 @@
-# A placeholder for GPT model architecture class
-import torch
+import torch   
 import torch.nn as nn
 
 GPT_CONFIG_124M = {
@@ -19,8 +18,11 @@ class DummyGPTModel(nn.Module):
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
         self.drop_emb = nn.Dropout(cfg["drop_rate"])
         
+        # Use a placeholder for TransformerBlock
         self.trf_blocks = nn.Sequential(
             *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        
+        # Use a placeholder for LayerNorm
         self.final_norm = DummyLayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(
             cfg["emb_dim"], cfg["vocab_size"], bias=False
@@ -41,32 +43,35 @@ class DummyGPTModel(nn.Module):
 class DummyTransformerBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
+        # A simple placeholder
 
     def forward(self, x):
+        # This block does nothing and just returns its input.
         return x
 
 
 class DummyLayerNorm(nn.Module):
     def __init__(self, normalized_shape, eps=1e-5):
         super().__init__()
+        # The parameters here are just to mimic the LayerNorm interface.
 
     def forward(self, x):
+        # This layer does nothing and just returns its input.
         return x
     
-# A layer normalization class
-class LayerNorm(nn.Module):
-    def __init__(self, emb_dim):
-        super().__init__()
-        self.eps = 1e-5
-        self.scale = nn.Parameter(torch.ones(emb_dim))
-        self.shift = nn.Parameter(torch.zeros(emb_dim))
+import tiktoken
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+# print(batch)
 
-    def forward(self, x):
-        mean = x.mean(dim=-1, keepdim=True)
-        var = x.var(dim=-1, keepdim=True, unbiased=False)
-        x_norm = (x - mean) / torch.sqrt(var + self.eps)
-        return self.scale * x_norm + self.shift
-    
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+
 # An implementation of the GELU activation function
 class GELU(nn.Module):
     def __init__(self):
@@ -77,40 +82,40 @@ class GELU(nn.Module):
             torch.sqrt(torch.tensor(2 / torch.pi)) * 
             (x + 0.044715 * torch.pow(x, 3))
         ))
-    
+# Example usage
+import matplotlib.pyplot as plt
+gelu, relu = GELU(), nn.ReLU()
+
+x = torch.linspace(-3, 3, 100)
+y_gelu, y_relu = gelu(x), relu(x)
+plt.figure(figsize=(8, 3))
+for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+    plt.subplot(1, 2, i)
+    plt.plot(x, y)
+    plt.title(f"{label} activation function")
+    plt.xlabel("x")
+    plt.ylabel(f"{label}(x)")
+    plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
 # A feed forward neural network module
 class FeedForward(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Linear(cfg["emb_dim"], 4 * cfg["ffwd_dim"]),
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
             GELU(),
             nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
         )
 
     def forward(self, x):
         return self.layers(x)
-    
-# A neural network to illustrate shortcut connections
-class ExampleDeepNeuralNetwork(nn.Module):
-    def __init__(self, layer_sizes, use_shortcut):
-        super().__init__()
-        self.use_shortcut = use_shortcut
-        self.layers = nn.ModuleList([
-            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), GELU()),
-            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), GELU()),
-            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), GELU()),
-            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), GELU()),
-            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), GELU())
-        ])
 
-    def forward(self, x):
-        for layer in self.layers:
-            # Compute the output of the current layer
-            layer_output = layer(x)
-            # Check if shortcut can be applied
-            if self.use_shortcut and x.shape == layer_output.shape:
-                x = x + layer_output
-            else:
-                x = layer_output
-        return x
+print(GPT_CONFIG_124M["emb_dim"])
+ffn = FeedForward(GPT_CONFIG_124M)
+
+# input shape: [batch_size, num_token, emb_size]
+x = torch.rand(2, 3, 768) 
+out = ffn(x)
+print(out.shape)
