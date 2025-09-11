69d1f2c2de7ca25cd7670701c7faea8bc44a687b
Jack <jackson11235813@163.com>
2025-09-11 15:33:59 +0800

    learn how to load and save weight in neural network

--- llm-scratch/chap5-pretraining/load-save-weight.py
+++ llm-scratch/chap5-pretraining/load-save-weight.py
@@ -287,14 +287,38 @@ def generate(model, idx, max_new_tokens, context_size,
     return idx
 
 # example usage
-torch.manual_seed(123)
-token_ids = generate(
-    model=model,
-    idx=text_to_token_ids("Every effort moves you", tokenizer),
-    max_new_tokens=15,
-    context_size=GPT_CONFIG_124M["context_length"],
-    top_k=25,
-    temperature=0.5,
-)
-print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+# torch.manual_seed(123)
+# token_ids = generate(
+#     model=model,
+#     idx=text_to_token_ids("Every effort moves you", tokenizer),
+#     max_new_tokens=15,
+#     context_size=GPT_CONFIG_124M["context_length"],
+#     top_k=25,
+#     temperature=0.5,
+# )
+# print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
 
+# save weights
+
+# new in this file: saving and loading model weights
+# torch.save(model.state_dict(), "model.pth")
+
+model = GPTModel(GPT_CONFIG_124M)
+model.load_state_dict(torch.load("model.pth", map_location=device))
+model.eval()
+
+# torch.save({
+#     "model_state_dict": model.state_dict(),
+#     "optimizer_state_dict": optimizer.state_dict(),
+#     },
+#     "model_and_optimizer.pth"
+# )
+
+checkpoint = torch.load("model_and_optimizer.pth", weights_only=True)
+
+model = GPTModel(GPT_CONFIG_124M)
+model.load_state_dict(checkpoint["model_state_dict"])
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0005, weight_decay=0.1)
+optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+model.train();
